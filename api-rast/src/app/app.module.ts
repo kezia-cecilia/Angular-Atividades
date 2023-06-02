ca959db5d3d3f4e5285661cfaff5417c583f0e6c
kezia-cecilia <keziacecilia5@gmail.com>
2023-06-02 16:45:36 -0300

    Aula 8

--- api-rast/src/app/app.module.ts
+++ api-rast/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ServicosService } from './service/servicos.service';
 import { AlunosComponent } from './alunos/alunos.component';
+import { LoginComponent } from './login/login.component';
+import { PainelComponent } from './painel/painel.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlunosComponent
+    AlunosComponent,
+    LoginComponent,
+    PainelComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
